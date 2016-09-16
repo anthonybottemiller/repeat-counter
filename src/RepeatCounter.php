61ddb8bfd28fecc1a193d91172d1c66522ec7594
Anthony Bottemiller <anthonybottemiller@outlook.com>
2016-09-16 11:15:09 -0700

    makes a variety of improvments to twig template home

--- src/RepeatCounter.php
+++ src/RepeatCounter.php
@@ -1,26 +1,27 @@
 <?php
 
-class RepeatCounter
-{
-    private $inputString;
-    private $comparisonString;
-    private $wordCount = 0;
-
-    function __construct($inputString, $comparisonString)
+    class RepeatCounter
     {
-      $this->inputString = $inputString;
-      $this->comparisonString = $comparisonString;
-    }
+        private $inputString;
+        private $comparisonString;
+        private $wordCount = 0;
 
-    function CountRepeats()
-    {
-      $this->inputString = explode(" ", $this->inputString);
-      foreach ($this->inputString as $string) 
-      {
-        if ($string == $this->comparisonString)        
-          $this->wordCount++;
-      }
-      return $this->wordCount;
+        function __construct($inputString, $comparisonString)
+        {
+          $this->inputString = $inputString;
+          $this->comparisonString = $comparisonString;
+        }
+
+        function CountRepeats()
+        {
+          $this->inputString = explode(" ", $this->inputString);
+          foreach ($this->inputString as $string) 
+          {
+            if ($string == $this->comparisonString)        
+              $this->wordCount++;
+          }
+          return $this->wordCount;
+        }
     }
-}
+    
 ?>
